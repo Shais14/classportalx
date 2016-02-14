511ff90adef546c2e89140ebcce151b4f36ef931
wumpafruit <jesankar@ncsu.edu>
2016-02-14 21:27:56 +0000

    Added user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ClassPortal::Application.routes.draw do
   get "static_pages/home"
   get "users/new"
   get 'signup'  => 'users#new'
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
