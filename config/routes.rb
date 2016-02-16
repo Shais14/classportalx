7564b1513ff734927694d3e0d68b5725b2e2b0b7
wumpafruit <jesankar@ncsu.edu>
2016-02-16 22:09:07 +0000

    Edit profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 ClassPortal::Application.routes.draw do
   get 'sessions/new'
 
+  get 'sessions/new'
+
   root 'static_pages#home'
   get "static_pages/home"
   get "users/new"
+  get "users/edit"
   get 'signup'  => 'users#new'
+  get 'edit' => 'users#edit'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
