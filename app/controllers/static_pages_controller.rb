ef1b203f7a6226b563a514aee99e91d80d55b1e5
wumpafruit <jesankar@ncsu.edu>
2016-02-17 02:18:19 +0000

    Fixed dropdown and added some redirecting

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,27 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      if current_user.privilege == "admin" || current_user.privilege == "superadmin"
+        redirect_to static_pages_admin_url
+      elsif current_user.privilege == "instructor"
+        redirect_to static_pages_instructor_url
+      elsif current_user.privilege == "student"
+        redirect_to static_pages_student_url
+      end
+    end
   end
   def admin
     if !logged_in? || (current_user.privilege == "admin" && !current_user.privilege == "superadmin")
       redirect_to static_pages_home_url
     end
   end
+  
+  def instructor
+    if !logged_in? || !current_user.privilege == "instructor"
+      redirect_to static_pages_home_url
+    end
+  end
+  
   def student
     if !logged_in? || !current_user.privilege == "student"
       redirect_to static_pages_home_url
