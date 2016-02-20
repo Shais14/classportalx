4da90b76038f3f66d432c2e8a834d705c484b9e9
wumpafruit <jesankar@ncsu.edu>
2016-02-20 01:33:49 +0000

    Changed grades table to hold student course information

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -84,7 +84,7 @@ class CoursesController < ApplicationController
   
   def grades
     if logged_in? && (current_user.type == "Admin" || current_user.type == "SuperAdmin" || current_user.type == "Instructor")
-      @grades = Grade.where("course_id = ?", params[:course_id])
+      @students_courses = StudentsCourses.where("course_id = ?", params[:course_id])
     else
       redirect_to static_pages_home_url
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,20 +6,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
-<<<<<<< HEAD
-  def new
-    @user = User.new
-  end
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:success] = "Welcome to the Class Portal!"
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
-=======
   #Can't create a user without a privilege
   #def new
   #  @user = User.new
@@ -33,7 +19,6 @@ class UsersController < ApplicationController
   #    render 'new'
   #  end
   #end
->>>>>>> a10f98fb6359441795f17285368d209ad92fbd76
   
   def edit
     @user = User.find(params[:id])
